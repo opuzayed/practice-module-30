77417eca2330e30c537a44341d705d6c6a19293b
opuzayed <zayedopu4933@gmail.com>
2022-08-25 11:26:04 +0600

    arrow function no parameter use

--- js/arrow.js
+++ js/arrow.js
@@ -6,4 +6,8 @@ const fullName = getFullName('zayed', 'opu');
 //console.log(fullName);
 let number = (a, b) => a * b;
 let result = number(12, 12);
-console.log(result);
+//console.log(result);
+//no parameter
+let pie = () => 3.1416;
+let value = pie();
+console.log(value);
