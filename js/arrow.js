baa79d004e3f8f65ad09c88bd5009e88551ff205
opuzayed <zayedopu4933@gmail.com>
2022-08-25 11:21:42 +0600

    arrow function use

---
+++ js/arrow.js
@@ -0,0 +1,9 @@
+const add = (num1, num2) => num1 + num2;
+//const result = add(2, 4);
+//console.log(result);
+const getFullName = (first, last) => first + ' ' + last;
+const fullName = getFullName('zayed', 'opu');
+//console.log(fullName);
+let number = (a, b) => a * b;
+let result = number(12, 12);
+console.log(result);
