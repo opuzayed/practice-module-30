97d0ec9e3dc64776f0060a3f0421bdbcb9c78c6c
opuzayed <zayedopu4933@gmail.com>
2022-08-29 14:47:29 +0600

    Create dynamic url based on click and display data

--- js/arrow.js
+++ js/arrow.js
@@ -517,13 +517,13 @@ const loadMeals = (search) => {
 };
 const displayMeals = (meals) => {
   const mealContainer = document.getElementById("meal-container");
-  mealContainer.innerText = '';
-  meals.forEach(meal => {
+  mealContainer.innerText = "";
+  meals.forEach((meal) => {
     console.log(meal);
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-    <div class="card h-100">
+    <div onclick="loadMealDetail(${meal.idMeal})"class="card h-100">
     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${meal.strMeal}</h5>
@@ -532,13 +532,34 @@ const displayMeals = (meals) => {
   </div>
    `;
     mealContainer.appendChild(mealDiv);
-  })
+  });
 };
-const searchFood = () =>{
-  const searchField = document.getElementById('search-field');
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadMeals(searchText);
-  searchField.value = '';
-  
-}
-//loadMeals('burger');
+  searchField.value = "";
+};
+//--------------------- Create dynamic url based on click and display data-------
+
+const loadMealDetail = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetail(data.meals[0]));
+};
+const displayMealDetail = (meal) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = ``;
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("card");
+  mealDiv.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">${meal.strMeal}</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+  `;
+  detailContainer.appendChild(mealDiv);
+};
